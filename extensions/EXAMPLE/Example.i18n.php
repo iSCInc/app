8da452a48669f5b927358b865352160ff9d17e47
Suriyaa Kudo <isc.suriyaa@gmail.com>
2014-06-18 12:34:58 +0200

    Create Example.i18n.php

---
+++ extensions/EXAMPLE/Example.i18n.php
@@ -0,0 +1,19 @@
+<?php
+
+// initialize the messages variable
+$messages = array();
+
+// English
+$messages[ 'en' ] = array(
+	'example-example' => 'This is an example!',
+);
+
+// German
+$messages[ 'de' ] = array(
+	'example-example' => 'Das ist nur ein Beispiel!',
+);
+
+// Message documentation for translators
+$messages[ 'LANG' ] = array(
+	'example-example' => 'TEXT',
+);
